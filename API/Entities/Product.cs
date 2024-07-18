3f94153e7775565d0dfc59dd30874b6906a61bae
jugovicivana <jugovicivana12@gmail.com>
2024-07-18 19:51:34 +0200

    commit

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -4,6 +4,9 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; }
+        public string PictureUrl { get; set; }
         
     }
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9410f2abf6970581b2402f873486a1097850fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db062ff6e0acfc3abacff26a9459854a538c97ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
