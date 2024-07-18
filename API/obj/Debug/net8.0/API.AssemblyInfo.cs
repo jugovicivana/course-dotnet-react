db062ff6e0acfc3abacff26a9459854a538c97ab
jugovicivana <jugovicivana12@gmail.com>
2024-07-18 19:40:02 +0200

    i

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56b51f8f510677ec6289d6c525ba45509d6487b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e9410f2abf6970581b2402f873486a1097850fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
